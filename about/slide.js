d568191e632a2f64699dbaed4526a22a1c8c5689
Joessyeon <joseputradion@gmail.com>
2025-06-10 10:03:03 +0700

    cleanin

--- about/slide.js
+++
@@ -1,6 +0,0 @@
-
-    let layer = document.querySelector(".layer");
-    window.addEventListener("scroll", function () {
-      let value = window.scrollY;
-      layer.style.left = value + "px"; //70px
-    });
